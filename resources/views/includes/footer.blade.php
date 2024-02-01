aebebb26b50bff41152461274be2aadb166c0bfe
Alex Munteanu <vootehdoo@gmail.com>
2024-02-01 19:20:27 +0100

    Swap footer <p>

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -1,8 +1,8 @@
 <footer>
     <div>
-        <p>@TicketEase - made by <a href="https://github.com/voothedoo">Alexandru Munteanu</a></p>
         @auth()
             <p>Logged In as <span>{{ auth()->user()->name }}</span></p>
         @endauth
+        <p>@TicketEase - made by <a href="https://github.com/voothedoo">Alexandru Munteanu</a></p>
     </div>
 </footer>

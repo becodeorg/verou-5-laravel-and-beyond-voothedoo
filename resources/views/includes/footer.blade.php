77a2efdeb5d44debcccbaedfb13cab548ac600cd
Alex Munteanu <vootehdoo@gmail.com>
2024-02-01 19:24:00 +0100

    Add a <p> for unknown (not logged in) users

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -3,6 +3,9 @@
         @auth()
             <p>Logged In as <span>{{ auth()->user()->name }}</span></p>
         @endauth
+        @guest
+            <p>Unknown User - Limited functionality for unknown users </p>
+        @endguest
         <p>@TicketEase - made by <a href="https://github.com/voothedoo">Alexandru Munteanu</a></p>
     </div>
 </footer>
